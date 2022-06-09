5ee08fc893550dbcc3ce58a0e299ef9e792d6fae
Darlan Talisson <darlan-talisson@hotmail.com>
2022-06-09 08:31:47 -0300

    s

--- js/teste/script.js
+++ js/teste/script.js
@@ -1,18 +1,26 @@
-    var fsex = document.getElementsByName('radsex')
-    var res = document.getElementById('res')
+var img1 = document.getElementById('hero')
+var Perso1 = document.getElementById('phoenix')
+var Perso3 = document.getElementById('jett')
+var Perso2 = document.getElementById('raze')
+var Perso4 = document.getElementById('fade')
+var fsex = document.getElementsByName('radsex')
 
-    var genero = ''
-    var img = document.createElement('img')
 
-function checar1() { 
-    img.setAttribute('id','foto')
-    if (fsex[0].checked) {
-        genero ='homem' 
-        img.setAttribute('src','leon.png.jpg')
+function verificar() { 
+    if ( fsex[0].checked) { 
+        img1.style.backgroundImage='url(img/phoenix2.png)'
+        img1.style.transitionDuration='1s'
     }
-    else if (fsex[1].checked) { 
-        genero='mulher'
-        img.setAttribute('src','ashley.png.jpg')
+    else if ( fsex[1].checked) { 
+        img1.style.backgroundImage='url(img/jett.png)'
+        img1.style.transitionDuration='1s'
     }
-    res.appendChild(img)    
+    else if (fsex[2].checked) { 
+        img1.style.backgroundImage='url(img/raze.png)'
+        img1.style.transitionDuration='1s'
+    }
+    else if (fsex[3].checked) { 
+        img1.style.backgroundImage='url(img/fade.png)'
+        img1.style.transitionDuration='1s'
+    }
 }
